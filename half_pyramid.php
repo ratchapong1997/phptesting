276c7843570810ae85a2a2815bba4a92c4fc9c95
ratchapong1997 <59477416+ratchapong1997@users.noreply.github.com>
2020-07-13 08:39:47 +0700

    Create half_pyramid.php

---
+++ half_pyramid.php
@@ -0,0 +1,38 @@
+<?php
+$number = 5;
+half_pyramid($number);
+
+//แบบที่ 1 *ใช้ font color เพราะ ลองเว้นวรรคธรรมดาแล้วมีปัญหา
+function half_pyramid($n){ 
+
+  for ($i = 0; $i < $n; $i++){
+	
+	for($k = $n; $k > $i+1; $k--){
+		echo "<font style='color:#ffffff'>"."O"."O"."</font>";
+		}
+	
+	for($j = 0 ; $j <= $i; $j++){
+		echo "O"."<font style='color:#ffffff'>"."O"."</font>";		
+		}
+	
+	echo "<br>";
+	}
+}
+
+//แบบที่ 2 ใช้เว้นวรรคธรรมดา
+function half_pyramid2($n){ 
+
+  for ($i = 0; $i < $n; $i++){
+	
+	for($k = $n; $k > $i+1; $k--){
+		echo " "." ";
+		}
+	
+	for($j = 0 ; $j <= $i; $j++){
+		echo "O"." ";		
+		}
+	
+	echo "<br>";
+	}
+}
+?>
