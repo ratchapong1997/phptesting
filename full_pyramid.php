ffa4e8447323fe0f0102ad6d4443d2d239a097e8
ratchapong1997 <59477416+ratchapong1997@users.noreply.github.com>
2020-07-13 08:40:14 +0700

    Create full_pyramid.php

---
+++ full_pyramid.php
@@ -0,0 +1,57 @@
+<?php
+
+$number = 5;
+full_pyramid($number);
+
+//แบบที่ 1 ใช้ font color *เพราะลองเว้นวรรคธรรมดาแล้วมีปัญหา
+function full_pyramid($n){
+
+  for ($i = 0; $i < $n; $i++){
+	
+	for($k = $n; $k > $i+1; $k--){
+		echo "<font style='color:#ffffff'>"."O"."O"."</font>";
+		}
+	
+	for($j = 0 ; $j <= $i; $j++){
+		echo "O"."<font style='color:#ffffff'>"."O"."</font>";		
+		}
+	
+	for($l = $n; $l < $i; $l--){
+		echo "<font style='color:#ffffff'>"."O"."O"."</font>";
+		}
+	
+	for($m = 1 ; $m <= $i; $m++){
+		echo "O"."<font style='color:#ffffff'>"."O"."</font>";		
+		}
+	
+	echo "<br>";
+	}
+}
+
+//แบบที่ 2 ใช้เว้นวรรคธรรมดา
+function full_pyramid2($n){
+	
+  for ($i = 0; $i < $n; $i++){
+	
+	for($k = $n; $k > $i+1; $k--){
+		echo " "." ";
+		}
+	
+	for($j = 0 ; $j <= $i; $j++){
+		echo "O"." ";	
+		}
+	
+	for($l = $n; $l < $i; $l--){
+		echo " "." ";
+		}
+	
+	for($m = 1 ; $m <= $i; $m++){
+		echo "O"." ";		
+		}
+	
+	echo "<br>";
+	}
+}
+
+
+?>
